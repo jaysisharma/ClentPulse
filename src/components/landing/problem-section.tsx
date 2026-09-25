9aa252cce129858447aafe66bc66f1d1709707ca
Jaysi <study.jaysi@gmail.com>
2026-09-25 16:52:09 +0545

    style: shift Sounds familiar annotation higher up and further left

--- src/components/landing/problem-section.tsx
+++ src/components/landing/problem-section.tsx
@@ -210,7 +210,7 @@ export function ProblemSection() {
               {/* Handwritten "Sounds familiar?" Annotation & Curved Arrow */}
               <div
                 ref={annotationRef}
-                className="absolute -left-3 sm:-left-6 lg:-left-10 top-1/2 -translate-y-12 flex flex-col items-center z-30 select-none pointer-events-none"
+                className="absolute -left-6 sm:-left-12 lg:-left-18 top-1/2 -translate-y-24 flex flex-col items-center z-30 select-none pointer-events-none"
               >
                 <span
                   style={{ fontFamily: 'var(--font-caveat), cursive' }}
